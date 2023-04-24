3723dc3f725b04e93f9d938db58fb848f58f7ca6
Daniela Morariu <daniela.morariu@target-e.com>
2023-04-24 19:26:44 +0300

    Display url as link

--- src/index.js
+++ src/index.js
@@ -56,15 +56,23 @@ function updateTeamRequest(team) {
 }
 
 function getTeamAsHTML(team) {
+  const id = team.id;
+  const url = team.url;
+  let displayUrl = url;
+
+  if (url.startsWith("https://")) {
+    displayUrl = url.substring(8);
+  }
+
   return `
     <tr>
       <td>${team.promotion}</td>
       <td>${team.members}</td>
       <td>${team.name}</td>
-      <td>${team.url}</td>
+      <td><a href="${url}" target="_blank">${displayUrl}</a></td>
       <td>
-        <a data-id="${team.id}" class="link-btn remove-btn">✖️</a>
-        <a data-id="${team.id}" class="link-btn edit-btn">✎</a>
+        <a data-id="${id}" class="link-btn remove-btn">✖️</a>
+        <a data-id="${id}" class="link-btn edit-btn">✎</a>
       </td>
     </tr>`;
 }
